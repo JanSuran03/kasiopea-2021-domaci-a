be6cc416afd394983a255143afe5abd6e1a3d1a5
JanSuran03 <62071148+JanSuran03@users.noreply.github.com>
2021-11-20 12:57:35 +0100

    Initial commit

---
+++ src/kasiopea_2021_domaci_a/core.clj
@@ -0,0 +1,25 @@
+(ns kasiopea-2021-domaci-a.core
+  (:require [clojure.string :as str]))
+
+(def coins-vals [1 2 5 10 20 50])
+
+(defn read-input []
+  (->> "input.txt" slurp
+       str/split-lines
+       rest
+       (partition 2)
+       (map (fn [[cost coins]]
+              (let [split-coins (->> (str/split coins #"\ ")
+                                     (map read-string))]
+                [(read-string cost) split-coins])))))
+
+(defn -main [& args]
+  (let [input (read-input)
+        calculated (map (fn [[cost coins]]
+                          (let [total-coins (apply + (map * coins coins-vals))]
+                            (if (>= total-coins cost)
+                              "ANO"
+                              "NE")))
+                        input)]
+    (->> calculated (str/join "\n")
+         (spit "output.txt"))))
